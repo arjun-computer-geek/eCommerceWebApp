5d6d5d059d7f44b33bbaf711cb6f0f88aa601555
Arjun Kumar <arjun2000raj@gmail.com>
2021-04-03 22:16:19 +0530

    adding get a product route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,3 +21,20 @@ exports.getProducts = async (req, res,next) => {
         message: products
     });
 }
+
+//Get Single Product Details => /api/v1/product/:id
+exports.getSingleProduct = async(req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product Not found'
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProducts,createProduct} = require('../controllers/productController');
+const {getProducts,createProduct,getSingleProduct} = require('../controllers/productController');
 
 router
     .route('/products')
     .get(getProducts);
-
+router
+    .route('/product/:id')
+    .get(getSingleProduct);
 router
     .route('/product/new')
     .post(createProduct);
