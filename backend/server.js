142ed4510e23bdd9256a9cd4a3abd3f6c186333f
Arjun Kumar <arjun2000raj@gmail.com>
2021-04-04 09:57:28 +0530

    fixed environment variable problem

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
 
 const app = require('./app');
-const connectDatabase = require('./config/database');
 
 
 //Setting up Config File
-dotenv.config({path: 'backend/config/config.env'});
+dotenv.config({path: './backend/config/config.env'});
+
+console.log(app.get('env'))
 
 //Connecting To Database
 connectDatabase();
 
 //Listning on Server
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on Port : ${process.env.PORT}`);
+    console.log(`Server is running on Port : ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
