aa5fad735b5ebe8068f823453cd0e2def3e5f2e0
Arjun <arjun2000raj@gmail.com>
2021-05-10 15:13:02 +0530

    Refractring frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-import Footer from './components/layout/Footer';
-import Home from './components/Home';
-import Navbar from './components/layout/Navbar';
-
-const App = () => {
-    return(
-        <>
-            <Router>
-                <Navbar />
-                <Route path="/" component={Home} exact />
-                <Footer/>
-           </Router>
-        </>
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
